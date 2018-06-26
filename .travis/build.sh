9fd7f5e3c951c1f7867743b2fbcb8320e32745fd
Hadrien Mary <hadrien.mary@gmail.com>
2018-06-26 14:16:07 -0400

    Enable upload-update-site.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,4 +2,4 @@
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh
 curl -fsLO https://raw.githubusercontent.com/fiji/fiji/master/bin/upload-update-site.sh
-#sh upload-update-site.sh
+sh upload-update-site.sh
