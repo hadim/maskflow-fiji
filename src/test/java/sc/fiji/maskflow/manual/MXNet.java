8b8905a6bcbdda29ae8083dba5b5f32c761508b7
Hadrien Mary <hadrien.mary@gmail.com>
2018-11-08 21:32:08 -0500

    bump scijava

---
+++ src/test/java/sc/fiji/maskflow/manual/MXNet.java
@@ -0,0 +1,13 @@
+
+package sc.fiji.maskflow.manual;
+
+import net.imagej.ImageJ;
+
+public class MXNet {
+
+	public static void main(String[] args) {
+
+		final ImageJ ij = new ImageJ();
+		ij.ui().showUI();
+	}
+}
